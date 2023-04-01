4cec90d89d31572d4008801bfe4318bf5b161ce8
Crono08 <jonathancalcanor@gmail.com>
2023-04-01 17:40:12 -0300

    Home terminado

---
+++ js/carousel-c.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__list-c'), {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        draggable: true,
+        dots: '.carousel__indicator-c',
+        arrows: {
+            prev: '.carousel__previous-c',
+            next: '.carousel__next-c'
+        },
+        responsive: [
+            {
+              // screens greater than >= 720px
+              breakpoint: 720,
+              settings: {
+                // Set to `auto` and provide item width to adjust to viewport
+                slidesToShow: '2',
+                slidesToScroll: '2',
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+              // screens greater than >= 1024px
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 3,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            }
+          ]
+    });
+});
